0174a2fb6a3d9f1a7e96a067ce4aa03818208dda
Kevin <bubbagump2020@gmail.com>
2020-04-06 15:54:04 -0500

    Able to pull data back from the CDC api

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import TexasMumps from './diseases/mumps/TexasMumps'
+
+const HomePage = () => {
+
+    
+
+    return(
+        <div>
+            <TexasMumps />
+        </div>
+    )
+}
+
+export default HomePage;
